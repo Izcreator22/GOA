c5bf8e1ac737a36701138f23fed816dd68e1c926
Izcreator22 <145269429+Izcreator22@users.noreply.github.com>
2024-07-04 13:45:27 +0400

    Add files via upload

---
+++ Python Course/Day74/Classwork/ZurabKuchava.script.js
@@ -0,0 +1,14 @@
+i = 0;
+if (v == a[i]){
+    printf("found value in place 5d/n", i);
+    return 0;
+}
+else {
+    if (i == n-1){
+        printf("Value not found/n");
+        return 1;
+    }
+    i = i + 1;
+ // goto LOOP;
+}
+
