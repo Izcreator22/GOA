2107b4271fd0079bf92766891e01c7bfb5b078d9
Izcreator22 <145269429+Izcreator22@users.noreply.github.com>
2024-10-03 15:52:28 +0400

    Add files via upload

---
+++ Python Course/Day98/Classwork/script.js
@@ -0,0 +1,27 @@
+const witelquda = () => {
+    console.log("witelquda")
+}
+
+const komble = () => {
+    console.log("komble")
+}
+
+witelquda()
+
+/* const timer = setTimeout(() => {
+    const person = {
+        name: "Mouse",
+    }
+    console.log("person", person)
+}, 3000); */
+
+const timer = setTimeout(() => {
+    console.log("morcha dro!")
+}, 3000);
+
+const vagonshnikebi = setTimeout(() => {
+    console.log("gamoushvi matarebeli")
+}, 1000)
+
+komble()
+
---
+++ Python Course/Day98/Homework/script.js
@@ -0,0 +1,53 @@
+// 1
+const khinkali = () => {
+    console.log("khinkali")
+}
+
+const mwvadi = () => {
+    console.log("mwvadi")
+}
+
+const gurneti = () => {
+    console.log("gurneti")
+}
+
+khinkali()
+
+const interval = setTimeout(() => {
+    console.log("qartuli tsarsuli!")
+}, 4000);
+
+const fasasf = setTimeout(() => {
+    console.log("ajdsqf fasasf")
+}, 2000)
+
+mwvadi()
+gurneti()
+
+
+// 2
+const shaqari = () => {
+    console.log("shaqari")
+}
+
+const marili = () => {
+    console.log("marili")
+}
+
+const soda = () => {
+    console.log("burtsi")
+}
+
+shaqari()
+
+const dilaTimer = setTimeout(() => {
+    console.log("dila mshvidobisa!")
+}, 5000)
+
+const dgeTimer = setTimeout(() => {
+    console.log("dge mshvidobisa!")
+}, 2500)
+
+marili()
+soda()
+
