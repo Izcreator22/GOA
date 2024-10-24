b57477934eaa4e92b75384e6a93246d990b76265
Izcreator22 <145269429+Izcreator22@users.noreply.github.com>
2024-10-24 16:53:45 +0400

    Add files via upload

---
+++ Python Course/Day106/Homework/script.js
@@ -0,0 +1,73 @@
+// For Loop
+/*
+// 1 Triangle
+for (let i = 1; i <= 11; i++){
+    let stars = ''
+    for (let j = 1; j <= i; j++){
+        stars += '*'
+    }
+    console.log(stars)
+}
+
+// 2 Rectangle(4x6)
+for (let i = 1; i <= 4; i++){
+    let stars = ' '
+    for (let j = 1; j <= 6; j++){
+        stars += '*'
+    }
+    console.log(stars)
+}
+
+// 3 Rectangle(4x12)
+for (let i = 1; i <= 4; i++){
+    let stars = ''
+    for (let j = 1; j <= 12; j++){
+        stars += '*'
+    }
+    console.log(stars)
+}
+*/
+
+
+// While Loop
+
+
+// 1 Triangle
+let  = 1
+while (i <= 11){
+    let stars = ''
+    let j = 1
+    while (j <= i){
+        stars += '*'
+        j++
+    }
+    console.log(stars)
+    i++
+}
+
+// 2 Rectangle(4x6)
+let  = 1
+while (i <= 4){
+    let stars = ''
+    let j = 1
+    while (j <= 6){
+        stars += "*"
+        j++
+    }
+    console.log(stars)
+    i++
+}
+
+// 3 Rectangle(4x12)
+let ii = 1
+while (i <= 4){
+    let stars = ''
+    let j = 1
+    while (j <= 12){
+        stars += '*'
+        j++
+    }
+    console.log(stars)
+    i++
+}
+
