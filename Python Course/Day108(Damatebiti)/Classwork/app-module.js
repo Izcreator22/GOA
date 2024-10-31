610b15e19f90aa05c194092384db94aa11c0f876
Izcreator22 <145269429+Izcreator22@users.noreply.github.com>
2024-10-31 15:41:06 +0400

    Delete Python Course/Day108(Damatebiti)/Classwork directory

--- Python Course/Day108(Damatebiti)/Classwork/app-module.js
+++
@@ -1,9 +0,0 @@
-
-const name1 = "Davit"
-const name2 = "Roba lomi"
-
-const myFunc = (data) => {
-    return data
-}
-
-module.exports = {name1, name2, myFunc}
--- Python Course/Day108(Damatebiti)/Classwork/second-file.js
+++
@@ -1,9 +0,0 @@
-const { name1, name2, myFunc } = require("./app")
-
-const newFunction = () => {
-    console.log("New function is running")
-}
-
-console.log(name1, name2);
-console.log(myFunc("Some data"))
-newFunction();
